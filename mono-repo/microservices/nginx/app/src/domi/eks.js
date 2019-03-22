7809f5b7da504a80d5411707d4553cf2b7a0b1b5
d.b.pilapil.jr <d.b.pilapil.jr@accenture.com>
2019-03-22 15:43:27 +0800

    all

--- mono-repo/microservices/nginx/app/src/domi/eks.js
+++ mono-repo/microservices/nginx/app/src/domi/eks.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-$("#submit_signup").click(function(){
-    if($("#eid_signup").val() == "" || $("#ig_signup").val() == "" || $("#level_signup").val() == "" || $("#capab_signup").val() == "")
-    {
-      alert("Please fill in the required fields!");
-      $("#myModal_Upload").modal('show');
-          }
-              else
-                  {
-                  //      $("#myModal_Upload").modal('hide');
-                  //      $("#myModal_SendReviewBtn").modal('show');
-                        console.log($("#eid_signup").val());
-                              list();
-                                  }
-                                    });
-
-                                      
-                                      function list() {
-                                          var eid = document.getElementById('eid_signup').value;
-                                              var ig = document.getElementById('ig_signup').value;
-                                                  var level = document.getElementById('level_signup').value;
-                                                      var capab = document.getElementById('capab_signup').value;
-                                                          document.getElementById('myText').innerHTML = eid;
-                                                              document.getElementById('myText').innerHTML = ig;
-                                                                  document.getElementById('myText').innerHTML = level;
-                                                                      document.getElementById('myText').innerHTML = capab;
-                                                                      }
-                                                                      
-=======
 $(document).ready(function() {
   var entriesElement = $("#list");
   var formElement = $("#signupForm");
@@ -77,4 +48,3 @@ $(document).ready(function() {
       });
   })();
 });
->>>>>>> cbaf76358f2511e065cd830755f516f03465902d
